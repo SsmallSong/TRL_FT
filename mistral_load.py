2312ab494da98469df2ab79b26c47f708bf4ae83
SsmallSong <3151273556@qq.com>
2024-04-27 13:23:21 +0800

    rm load

--- mistral_load.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
