4bc8818603fbccc85d303afbc57b367d79777d07
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-04-28 10:23:50 +0000

    1

--- mistral_rm_load.py
+++ mistral_rm_load.py
@@ -12,17 +12,17 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 tokenizer = AutoTokenizer.from_pretrained("Ray2333/reward-model-Mistral-7B-instruct-Unified-Feedback")
 model = AutoModelForSequenceClassification.from_pretrained("Ray2333/reward-model-Mistral-7B-instruct-Unified-Feedback")
-
+print("111111111")
 # Load model directly
 from transformers import AutoModel
 model = AutoModel.from_pretrained("OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5")
-
+print('2222222222')
 # Load model directly
 from transformers import AutoTokenizer, AutoModel
 
 tokenizer = AutoTokenizer.from_pretrained("openbmb/Eurus-RM-7b", trust_remote_code=True)
 model = AutoModel.from_pretrained("openbmb/Eurus-RM-7b", trust_remote_code=True)
-
+print('333333333')
 from datasets import load_dataset
 
 dataset = load_dataset("tatsu-lab/alpaca_eval")
