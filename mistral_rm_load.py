cfc8fcb576b31bdc3f5c085b2edd055359407dc8
SsmallSong <3151273556@qq.com>
2024-04-28 18:10:05 +0800

    load rm

--- mistral_rm_load.py
+++ mistral_rm_load.py
@@ -1,8 +1,28 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# # Load model directly
+# from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
-# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
-import llm_blender
-blender = llm_blender.Blender()
-blender.loadranker("llm-blender/PairRM") # load PairRM
+# # tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+# # model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+# import llm_blender
+# blender = llm_blender.Blender()
+# blender.loadranker("llm-blender/PairRM") # load PairRM
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained("Ray2333/reward-model-Mistral-7B-instruct-Unified-Feedback")
+model = AutoModelForSequenceClassification.from_pretrained("Ray2333/reward-model-Mistral-7B-instruct-Unified-Feedback")
+
+# Load model directly
+from transformers import AutoModel
+model = AutoModel.from_pretrained("OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5")
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("openbmb/Eurus-RM-7b", trust_remote_code=True)
+model = AutoModel.from_pretrained("openbmb/Eurus-RM-7b", trust_remote_code=True)
+
+from datasets import load_dataset
+
+dataset = load_dataset("tatsu-lab/alpaca_eval")
