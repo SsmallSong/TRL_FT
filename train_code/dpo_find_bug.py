b3a93e404e8e8e9ed248ff94c8d7273e570d0904
SsmallSong <3151273556@qq.com>
2024-05-03 22:28:08 +0800

    1

--- train_code/dpo_find_bug.py
+++ train_code/dpo_find_bug.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset
 from trl import SFTTrainer
 import torch
+from vllm import LLM, SamplingParams
 messages_1 = [
     {"role": "user", "content": "What is your favourite condiment?"},
    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
@@ -18,7 +19,9 @@ model_id="mistralai/Mistral-7B-Instruct-v0.2"
 #model_id='/home/wxt/huatong/huggingface/hub/mistral_7b_instruct_dpo'
 tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='left')
 print("Tokenizer Loading Finished!")
-model = AutoModelForCausalLM.from_pretrained(model_id).eval()
+# model = AutoModelForCausalLM.from_pretrained(model_id).eval()
+model = LLM(model=model_id, tensor_parallel_size=1,
+                  trust_remote_code=True)
 print("Model Loading Finished!")
 model.generation_config.pad_token_id = model.generation_config.eos_token_id
 
@@ -41,10 +44,10 @@ encodeds = tokenizer.apply_chat_template(messages_1, return_tensors="pt",tokeniz
 encodeds_2 = tokenizer.apply_chat_template(messages_1, return_tensors="pt",tokenize=False)
 print('enceodeds:',encodeds)
 print('enceodeds_2:',encodeds_2)
-encodeds = tokenizer.apply_chat_template(messages_2, return_tensors="pt",tokenize=True)
-encodeds_2 = tokenizer.apply_chat_template(messages_2, return_tensors="pt",tokenize=False)
-print('enceodeds:',encodeds)
-print('enceodeds_2:',encodeds_2)
+# encodeds = tokenizer.apply_chat_template(messages_2, return_tensors="pt",tokenize=True)
+# encodeds_2 = tokenizer.apply_chat_template(messages_2, return_tensors="pt",tokenize=False)
+# print('enceodeds:',encodeds)
+# print('enceodeds_2:',encodeds_2)
 
 model_inputs = encodeds.to(device)
 model.to(device)
