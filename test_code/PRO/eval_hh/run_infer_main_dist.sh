42eb2aa9f23d37ae120d64b0734a0c3e07e594f0
SsmallSong <3151273556@qq.com>
2024-05-08 23:39:52 +0800

    1

--- test_code/PRO/eval_hh/run_infer_main_dist.sh
+++ test_code/PRO/eval_hh/run_infer_main_dist.sh
@@ -5,12 +5,16 @@ id=$1
 ranking_len=$2
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
     --index $id \
-    --stage $ranking_len > logs/generate_infer_main_${id}_${ranking_len}.log 2>&1
+    --stage $ranking_len \
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/generate_infer_main_${id}_${ranking_len}.log 2>&1 \
+    
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
     --index $id \
-    --stage $ranking_len > logs/reward_infer_main_${id}_${ranking_len}.log 2>&1
+    --stage $ranking_len \
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/reward_infer_main_${id}_${ranking_len}.log 2>&1
 
 python -u infer_and_eval_main_score.py \
     --index $id \
-    --stage $ranking_len > logs/score_infer_main_${id}_${ranking_len}.log 2>&1
+    --stage $ranking_len \
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/score_infer_main_${id}_${ranking_len}.log 2>&1
