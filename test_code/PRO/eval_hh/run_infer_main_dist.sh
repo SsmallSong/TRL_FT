19209364182ef01e95ffe2011bd176e1dd05293b
SsmallSong <3151273556@qq.com>
2024-05-08 23:45:57 +0800

    1

--- test_code/PRO/eval_hh/run_infer_main_dist.sh
+++ test_code/PRO/eval_hh/run_infer_main_dist.sh
@@ -4,17 +4,17 @@ export OMP_NUM_THREADS=16
 id=$1
 ranking_len=$2
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index $id \
-    --stage $ranking_len \
+    --index no1 \
+    --stage 1 \
     --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/generate_infer_main_${id}_${ranking_len}.log 2>&1 \
     
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index $id \
-    --stage $ranking_len \
+    --index no1 \
+    --stage 1 \
     --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/reward_infer_main_${id}_${ranking_len}.log 2>&1
 
 python -u infer_and_eval_main_score.py \
-    --index $id \
-    --stage $ranking_len \
+    --index no1 \
+    --stage 1 \
     --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/score_infer_main_${id}_${ranking_len}.log 2>&1
