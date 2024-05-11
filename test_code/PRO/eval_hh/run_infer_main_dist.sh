2f9e9c5f174870c02be363897ff94cffc230706f
SsmallSong <3151273556@qq.com>
2024-05-11 21:58:54 +0800

    1

--- test_code/PRO/eval_hh/run_infer_main_dist.sh
+++ test_code/PRO/eval_hh/run_infer_main_dist.sh
@@ -6,15 +6,15 @@ ranking_len=$2
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
     --index no1 \
     --stage 1 \
-    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/generate_infer_main_${id}_${ranking_len}.log 2>&1 \
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/test_code/PRO/eval_hh/logs/generate_infer_main_${id}_${ranking_len}.log 2>&1 \
     
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
     --index no1 \
     --stage 1 \
-    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/reward_infer_main_${id}_${ranking_len}.log 2>&1
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/test_code/PRO/eval_hh/logs/reward_infer_main_${id}_${ranking_len}.log 2>&1
 
 python -u infer_and_eval_main_score.py \
     --index no1 \
     --stage 1 \
-    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > logs/score_infer_main_${id}_${ranking_len}.log 2>&1
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/test_code/PRO/eval_hh/logs/score_infer_main_${id}_${ranking_len}.log 2>&1
