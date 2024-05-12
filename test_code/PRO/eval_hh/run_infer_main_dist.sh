3176bc225a20574408d628f7ebe18de1172b3f16
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-05-12 04:27:39 +0000

    1

--- test_code/PRO/eval_hh/run_infer_main_dist.sh
+++ test_code/PRO/eval_hh/run_infer_main_dist.sh
@@ -4,17 +4,33 @@ export OMP_NUM_THREADS=16
 id=$1
 ranking_len=$2
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index no1 \
+    --index origin  \
     --stage 1 \
-    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_${id}_${ranking_len}.log 2>&1 \
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_origin.log 2>&1 \
     
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index no1 \
+    --index origin \
     --stage 1 \
-    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_${id}_${ranking_len}.log 2>&1
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_origin.log 2>&1
 
 python -u infer_and_eval_main_score.py \
-    --index no1 \
+    --index origin \
     --stage 1 \
-    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_${id}_${ranking_len}.log 2>&1
+    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_origin.log 2>&1
+
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+	    --index dpo  \
+	    --stage 2 \
+	    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo.log 2>&1 
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+	    --index dpo \
+	        --stage 2 \
+		    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo.log 2>&1
+
+python -u infer_and_eval_main_score.py \
+	    --index dpo \
+	        --stage 2 \
+		    --directory  /home/wxt/huatong/huggingface/hub/llama-7b-hh-sft > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo.log 2>&1
