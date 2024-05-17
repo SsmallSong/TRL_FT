1cdfc743757e8bc8c9e855dbead4b5b25cff9e6e
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-05-17 02:33:46 +0000

    1

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -4,28 +4,28 @@ export OMP_NUM_THREADS=16
 id=$1
 ranking_len=$2
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index dpo_beta0  \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo_beta0.log 2>&1 
+    --index dpo_beta001  \
+    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo_beta001.log 2>&1 
     
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index dpo_beta0 \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo_beta0.log 2>&1
+    --index dpo_beta001 \
+    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo_beta001.log 2>&1
 
 python -u infer_and_eval_main_score.py \
-    --index dpo_beta0 \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo_beta0.log 2>&1
+    --index dpo_beta001 \
+    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo_beta001.log 2>&1
 
 
 
-accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index dpo_beta03  \
-    --stage 2 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo_beta03.log 2>&1 
+#accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+ #   --index dpo_beta03  \
+  #  --stage 2 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo_beta03.log 2>&1 
     
-accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index dpo_beta03 \
-    --stage 2 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo_beta03.log 2>&1
+#accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+#    --index dpo_beta03 \
+#    --stage 2 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo_beta03.log 2>&1
 
-python -u infer_and_eval_main_score.py \
-    --index dpo_beta03 \
-    --stage 2 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo_beta03.log 2>&1
+#python -u infer_and_eval_main_score.py \
+#    --index dpo_beta03 \
+#    --stage 2 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo_beta03.log 2>&1
