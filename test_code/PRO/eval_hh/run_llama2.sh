cca296d407922c06435c51670f2a9d283a8f452b
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-05-22 08:15:40 +0000

    1

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -4,17 +4,17 @@ export OMP_NUM_THREADS=16
 id=$1
 ranking_len=$2
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index kto_beta01  \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_kto_beta01.log 2>&1 
+    --index dpo_beta01_e2  \
+    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo_beta01_e2.log 2>&1 
     
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index kto_beta01 \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_kto_beta01.log 2>&1
+    --index dpo_beta01_e2 \
+    --stage 1 >  /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo_beta01_e2.log 2>&1
 
 python -u infer_and_eval_main_score.py \
-    --index kto_beta01 \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_kto_beta01.log 2>&1
+    --index dpo_beta01_e2 \
+    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo_beta01_e2.log 2>&1
 
 
 
