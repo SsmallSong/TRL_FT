30598865b8eabdfe27897cd04bd8e4f6f896ca7d
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-05-22 09:11:11 +0000

    1

--- run_model/mistral_renmin.py
+++ run_model/mistral_renmin.py
@@ -3,14 +3,16 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset
 from trl import SFTTrainer
 import torch
-device = "cuda" # the device to load the model onto
+#device = "cuda" # the device to load the model onto
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+
 # export CUDA_VISIBLE_DEVICES=1
 model_id='/home/wxt/huatong/huggingface/hub/mistral_7b_instruct'
 model_id='mistralai/Mistral-7B-Instruct-v0.2'
 # model_id='daryl149/llama-2-7b-hf'
 tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='left')
 print("Tokenizer Loading Finished!")
-model = AutoModelForCausalLM.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map=device)
 print("Model Loading Finished!")
 
 model.generation_config.pad_token_id = model.generation_config.eos_token_id
@@ -27,19 +29,23 @@ messages_2 = [
     {"role": "user", "content": "2024年3月18日,习近平总书记在湖南考察期间第一站来到了哪所学校？"}
    ]
 
-encodeds = tokenizer.apply_chat_template(messages_1, return_tensors="pt")
+messages_1 = [
+            {"role": "user", "content": "On March 18, 2024, during his inspection tour in Hunan Province, General Secretary Xi Jinping made his first stop at which school?"},
 
-model_inputs = encodeds.to(device)
-model.to(device)
+               ]
+model_inputs= tokenizer.apply_chat_template(messages_1, return_tensors="pt").to(device)
 
-generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+#model_inputs = encodeds.to(device)
+#model.to(device)
+
+generated_ids = model.generate(model_inputs, max_new_tokens=200, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
 print(decoded[0])
+kill
+model_inputs = tokenizer.apply_chat_template(messages_2, return_tensors="pt")
 
-encodeds = tokenizer.apply_chat_template(messages_2, return_tensors="pt")
-
-model_inputs = encodeds.to(device)
-model.to(device)
+#model_inputs = encodeds.to(device)
+#model.to(device)
 
 generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
