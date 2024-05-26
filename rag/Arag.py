4d9cb675b5eda11b87c8b4d3a55f33e6e04d8cf9
SsmallSong <3151273556@qq.com>
2024-05-26 21:05:21 +0800

    1

--- rag/Arag.py
+++ rag/Arag.py
@@ -32,10 +32,11 @@ query_wrapper_prompt= SimpleInputPrompt("<USER|>{query_string}<|ASSISTANT>")
 
 modelid='mistralai/Mistral-7B-Instruct-v0.2'
 modelid="itpossible/Chinese-Mistral-7B-Instruct-v0.1"
-# modelid="baichuan-inc/Baichuan2-7B-Chat"
+modelid="baichuan-inc/Baichuan2-7B-Chat"
 llm = HuggingFaceLLM(
     context_window=1024,
     max_new_tokens=512,
+    trust_remote_code=True,
     generate_kwargs={"pad_token_id": 2,
             "temperature": 0.2, "do_sample": True},
 #    system_prompt=system_prompt,
