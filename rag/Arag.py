09bbb2424e7d5cbfaaf0dfed9ba03e200e137fb7
SsmallSong <3151273556@qq.com>
2024-05-26 23:56:11 +0800

    1

--- rag/Arag.py
+++ rag/Arag.py
@@ -28,31 +28,48 @@ system_prompt="""
 """
 
 
-query_wrapper_prompt= SimpleInputPrompt("<USER|>{query_string}<|ASSISTANT>")
 
 modelid='mistralai/Mistral-7B-Instruct-v0.2'
 modelid="itpossible/Chinese-Mistral-7B-Instruct-v0.1"
 modelid="baichuan-inc/Baichuan2-7B-Chat"
+
+# modelid="baichuan-inc/Baichuan2-13B-Chat"
 llm = HuggingFaceLLM(
     context_window=1024,
     max_new_tokens=512,
- #   trust_remote_code=True,
     generate_kwargs={"pad_token_id": 2,
             "temperature": 0.2, "do_sample": True},
  #   system_prompt=system_prompt,
-#    query_wrapper_prompt=query_wrapper_prompt,
     tokenizer_name=modelid,
     model_name=modelid,
     device_map="auto",
     model_kwargs={"trust_remote_code":True,"torch_dtype": torch.float16},
     tokenizer_kwargs={"trust_remote_code":True}
-    #model_kwargs={"torch_dtype": torch.float16 , "load_in_8bit":True}
+
 )
 
 
 def Mistral_instruct_query(questionText):
-#    questionText = "<s>[INST] " + questionText + " [/INST]"
-  #  questionText="<reserved_106> "+questionText
+    questionText = "<s>[INST] " + questionText + " [/INST]"
+    return questionText
+
+def Baichuan_instruct_query(questionText):
+    questionText="<reserved_106>"+questionText #+ "<reserved_107>"
+    return questionText
+
+def Origin_instruct_query(questionText):
+    return questionText
+
+def Chat_instruct_query(questionText,modelid,use_chat=True):
+    if use_chat==False:
+        questionText=Origin_instruct_query(questionText)
+    else:
+        if modelid.split('/')[0]=="baichuan-inc":
+            questionText=Baichuan_instruct_query(questionText)
+        elif modelid.split('/')[0]=="mistralai" or modelid.split('/')[0]=="itpossible":
+            questionText=Mistral_instruct_query(questionText)
+        else:
+            questionText=Origin_instruct_query(questionText)
     return questionText
 
 # embed_model=LangchainEmbedding(
@@ -60,6 +77,7 @@ def Mistral_instruct_query(questionText):
 
 embed_model=LangchainEmbedding(
     HuggingFaceEmbeddings(model_name="BAAI/bge-small-zh-v1.5"))
+
 Settings.embed_model = embed_model
 Settings.llm = llm
 
@@ -86,21 +104,15 @@ print("Finish Index")
 #                     )
 query_engine=index.as_query_engine()
 
-response=query_engine.query(Mistral_instruct_query("2024年是中国红十字会成立多少周年?"))
-print(response)
+query_list=[
+    "2024年是中国红十字会成立多少周年?",
+    "《中华人民共和国爱国主义教育法》什么时候实施？",
+    "2024年3月18日,习近平总书记在湖南考察期间第一站来到了哪所学校？",
+    "2024年我国文化和旅游部部长是谁？",
+    "2023—2024赛季国际滑联短道速滑世界杯北京站比赛中，刘少昂参与获得几枚奖牌？",
+    "福建自贸试验区在自贸建设十年中主要从哪几个方面推动改革创新？"
+]
 
-response=query_engine.query(Mistral_instruct_query("《中华人民共和国爱国主义教育法》什么时候实施？"))
-print(response)
-
-response=query_engine.query(Mistral_instruct_query("2024年3月18日,习近平总书记在湖南考察期间第一站来到了哪所学校？"))
-print(response)
-
-response=query_engine.query(Mistral_instruct_query("2024年我国文化和旅游部部长是谁？"))
-print(response)
-
-response=query_engine.query(Mistral_instruct_query("2023—2024赛季国际滑联短道速滑世界杯北京站比赛中，刘少昂参与获得几枚奖牌？"))
-print(response)
-
-response=query_engine.query(Mistral_instruct_query("福建自贸试验区在自贸建设十年中主要从哪几个方面推动改革创新？"))
-print(response)
-
+for i in range(len(query_list)):
+    response=query_engine.query(Chat_instruct_query(query_list[i],modelid,use_chat=False))
+    print(response)
