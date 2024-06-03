78695f2da900338117b47def6dec483eae56baa2
Song Huatong <3151273556@qq.com>
2024-06-03 22:20:05 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -1,9 +1,27 @@
 export PYTHONIOENCODING=utf-8
 export OMP_NUM_THREADS=16
 
-index="llama2_7b_sft_halos_2_3"
+index="llama2_7b_ppo_halos_2 "
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
 
+# 执行命令并重定向输出到动态生成的文件名
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+    --index $index  \
+    --stage 1 > $log_file 2>&1 
+    
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1
+
+python -u infer_and_eval_main_score.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1
+    
+index="llama2_7b_sft_halos_2_3"
+log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
+kill
 # 执行命令并重定向输出到动态生成的文件名
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
@@ -56,20 +74,4 @@ python -u infer_and_eval_main_score.py \
     --index $index \
     --stage 1 > $log_file 2>&1
 
-index="llama2_7b_ppo_halos_2 "
-log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
 
-# 执行命令并重定向输出到动态生成的文件名
-
-accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index $index  \
-    --stage 1 > $log_file 2>&1 
-    
-
-accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index $index \
-    --stage 1 > $log_file 2>&1
-
-python -u infer_and_eval_main_score.py \
-    --index $index \
-    --stage 1 > $log_file 2>&1
