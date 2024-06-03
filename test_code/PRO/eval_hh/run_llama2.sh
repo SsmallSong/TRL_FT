a74bb95b3ace771e8c8ad8fa41043e2d9ca26fe2
Song Huatong <3151273556@qq.com>
2024-06-03 19:56:51 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -1,20 +1,26 @@
 export PYTHONIOENCODING=utf-8
 export OMP_NUM_THREADS=16
 
-id=$1
-ranking_len=$2
+index="dpo_beta01_e2"
+log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_${index}.log"
+
+# 执行命令并重定向输出到动态生成的文件名
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-    --index dpo_beta01_e2  \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/generate_infer_main_dpo_beta01_e2.log 2>&1 
+    --index $index \
+    --stage 1 > $log_file 2>&1
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+    --index $index  \
+    --stage 1 > $log_file 2>&1 
     
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index dpo_beta01_e2 \
-    --stage 1 >  /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/reward_infer_main_dpo_beta01_e2.log 2>&1
+    --index $index \
+    --stage 1 > $log_file 2>&1
 
 python -u infer_and_eval_main_score.py \
-    --index dpo_beta01_e2 \
-    --stage 1 > /home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs/score_infer_main_dpo_beta01_e2.log 2>&1
+    --index $index \
+    --stage 1 > $log_file 2>&1
 
 
 
