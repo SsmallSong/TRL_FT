60d9de9d2e84b09f4a685474c580ee59eeb563d6
Song Huatong <3151273556@qq.com>
2024-06-04 01:24:49 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -1,7 +1,7 @@
 export PYTHONIOENCODING=utf-8
 export OMP_NUM_THREADS=16
 
-index="llama2_7b_ppo_halos_2"
+index="llama2_7b_dpo_halos_beta0"
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
 
 # 执行命令并重定向输出到动态生成的文件名
@@ -19,7 +19,7 @@ python -u infer_and_eval_main_score.py \
     --index $index \
     --stage 1 > $log_file 2>&1
     
-index="llama2_7b_sft_halos_2_3"
+index="llama2_7b_dpo_halos_beta001"
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
 kill
 # 执行命令并重定向输出到动态生成的文件名
@@ -38,7 +38,7 @@ python -u infer_and_eval_main_score.py \
     --stage 1 > $log_file 2>&1
 
 
-index="llama2_7b_dpo_halos_beta01"
+index="llama2_7b_dpo_halos_beta01_e2"
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
 
 # 执行命令并重定向输出到动态生成的文件名
@@ -56,7 +56,45 @@ python -u infer_and_eval_main_score.py \
     --index $index \
     --stage 1 > $log_file 2>&1
 
-index="llama2_7b_kto_halos_beta01"
+index="llama2_7b_dpo_halos_beta03"
+log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
+
+# 执行命令并重定向输出到动态生成的文件名
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+    --index $index  \
+    --stage 1 > $log_file 2>&1 
+    
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1
+
+python -u infer_and_eval_main_score.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1
+
+
+index="llama2_7b_kto_halos_beta01_lr25"
+log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
+
+# 执行命令并重定向输出到动态生成的文件名
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+    --index $index  \
+    --stage 1 > $log_file 2>&1 
+    
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1
+
+python -u infer_and_eval_main_score.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1
+
+    
+index="llama2_7b_kto_halos_lr56_beta01"
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_${index}.log"
 
 # 执行命令并重定向输出到动态生成的文件名
