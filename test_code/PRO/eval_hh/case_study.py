7e8ee9101ed47571ecd47271e9ac28c3f4793658
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-06-04 09:50:04 +0000

    1

--- test_code/PRO/eval_hh/case_study.py
+++ test_code/PRO/eval_hh/case_study.py
@@ -5,7 +5,8 @@ from transformers import (
     LlamaTokenizer,
     AutoModelForCausalLM
 )
-
+import os
+import torch
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
