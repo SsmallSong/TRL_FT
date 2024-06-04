fd8088ff4782ecf14d20af841b64c63fd294e673
Song Huatong <3151273556@qq.com>
2024-06-04 16:19:17 +0800

    Update infer_and_eval_main_generate.py

--- test_code/PRO/eval_hh/infer_and_eval_main_generate.py
+++ test_code/PRO/eval_hh/infer_and_eval_main_generate.py
@@ -45,6 +45,7 @@ if __name__ == "__main__":
     # model_name_or_path = os.path.join("..", "checkpoints", f"index_{args.index}", f"stage_{args.stage}", f"{args.directory}")
     model_name_or_path ='daryl149/llama-2-7b-hf'
     model_device = "cuda:{}".format(rank)
+    kill
 
     model_config = AutoConfig.from_pretrained(model_name_or_path)
     model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
