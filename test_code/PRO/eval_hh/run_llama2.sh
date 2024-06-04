10bb16fd9a40cfe9fb43e257489258903e358aee
Song Huatong <3151273556@qq.com>
2024-06-04 17:25:42 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -12,13 +12,13 @@ accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
     --model_ckpt $model_ckpt \
     --stage 1 > $log_file 2>&1 
     
-accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index $index \
-    --stage 1 > $log_file 2>&1
+# accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+#     --index $index \
+#     --stage 1 > $log_file 2>&1
 
-python -u infer_and_eval_main_score.py \
-    --index $index \
-    --stage 1 > $log_file 2>&1
+# python -u infer_and_eval_main_score.py \
+#     --index $index \
+#     --stage 1 > $log_file 2>&1
 
 
 # index="llama2_7b_dpo_halos_beta01_frontenter"
