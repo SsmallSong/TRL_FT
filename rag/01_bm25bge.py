3a68f72781b33048be0c834d1ad3907b509e8c3a
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-06-04 09:34:46 +0000

    1

--- rag/01_bm25bge.py
+++ rag/01_bm25bge.py
@@ -34,7 +34,7 @@ query_list=[
 model = FlagModel('BAAI/bge-large-zh-v1.5', 
                   query_instruction_for_retrieval="为这个句子生成表示以用于检索相关文章：",
                   use_fp16=True) # Setting use_fp16 to True speeds up computation with a slight performance degradation
-
+kill
 import gzip
 import json
 import pickle
