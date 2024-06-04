25700a06925888718cffe365869372a912412d29
Song Huatong <3151273556@qq.com>
2024-06-04 17:01:13 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -4,7 +4,7 @@ export OMP_NUM_THREADS=16
 index="llama2_7b_dpo_halos_beta01_noenter"
 # index="llama2_7b_dpo_halos_beta01_frontenter"
 # index="llama2_7b_dpo_halos_beta01_endenter"
-log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_llama2_{index}.log"
+log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_llama2_{$index}.log"
 model_ckpt="llama2_7b_dpo_halos_beta01"
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
