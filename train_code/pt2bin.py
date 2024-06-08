cb68044d71fbb15b97f869ad6448465fbc572d49
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-06-08 16:05:56 +0000

    1

--- train_code/pt2bin.py
+++ train_code/pt2bin.py
@@ -1,8 +1,18 @@
 import torch
-
+with open('/home/wxt/huggingface/hub/llama2_sft_mirror/pytorch_model-00001-of-00002.bin', 'rb') as file:
+        binary_data = file.read()
+num=0
+print(len(binary_data))
+for key,value in binary_data.items():
+    print(key)
+    print(value)
+    break
+kill
 # 加载policy.pt文件
 policy = torch.load('/home/wxt/huatong/huggingface/hub/llama2_7b_sft_halos_2_3/LATEST/policy.pt')
 cont=1
+print(len(policy))
+kill
 for key,value in policy.items():
     cont+=1
 
