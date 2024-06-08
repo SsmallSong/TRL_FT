381af883906d986d7ec9cc0dc815872a6680fbcb
Song Huatong <3151273556@qq.com>
2024-06-08 16:18:01 +0800

    Update check_code.py

--- train_code/check_code.py
+++ train_code/check_code.py
@@ -18,9 +18,9 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 import torch
 print(torch.cuda.device_count())
 
-model_name_or_path ='daryl149/llama-2-7b-hf'
-model_config = AutoConfig.from_pretrained(model_name_or_path )
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
+# model_name_or_path ='daryl149/llama-2-7b-hf'
+# model_config = AutoConfig.from_pretrained(model_name_or_path )
+# model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
 
 value_model = AutoModelForSequenceClassification.from_pretrained("OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5", num_labels=1)
 reward_model = AutoModelForSequenceClassification.from_pretrained("OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5", num_labels=1)
