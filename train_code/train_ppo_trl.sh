00f1145601542d768f640915322fa1313b1fbafa
Song Huatong <3151273556@qq.com>
2024-06-08 15:44:28 +0800

    Update train_ppo_trl.sh

--- train_code/train_ppo_trl.sh
+++ train_code/train_ppo_trl.sh
@@ -7,8 +7,8 @@ accelerate launch --config_file /home/wxt/huatong/TRL_FT/config_file/deepspeed_z
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --total_episodes 10000 \
-    --model_name_or_path daryl149/llama-2-7b-hf \
-    --sft_model_path daryl149/llama-2-7b-hf \
+    --model_name_or_path /home/wxt/huatong/huggingface/hub/models--daryl149--llama-2-7b-hf/snapshots/142d0a5354ab12acdfff745a4d5c2ced307970dd \
+    --sft_model_path /home/wxt/huatong/huggingface/hub/models--daryl149--llama-2-7b-hf/snapshots/142d0a5354ab12acdfff745a4d5c2ced307970dd \
     --reward_model_path OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5 \
     --local_rollout_forward_batch_size 1 \
     --non_eos_penalty 2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs.txt 
