bd26e5c5573988ef09de28e1b55c0f6ee62e67e2
WANG XITING <wxt@design-agent-09.iswoednucuwergeul1knj3ocmf.xx.internal.cloudapp.net>
2024-06-08 09:54:20 +0000

    1

--- train_code/check_code.py
+++ train_code/check_code.py
@@ -28,15 +28,15 @@ import torch
 print(torch.cuda.device_count())
 
 
-kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=5400))
-accelerator = Accelerator(kwargs_handlers=[kwargs])# **accelerator_log_kwargs)
-rank = int(os.environ['RANK'])
-rank_sum = accelerator.num_processes
-model_device = "cuda:{}".format(rank)
+#kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=5400))
+#accelerator = Accelerator(kwargs_handlers=[kwargs])# **accelerator_log_kwargs)
+#rank = int(os.environ['RANK'])
+#rank_sum = accelerator.num_processes
+#model_device = "cuda:{}".format(rank)
 
 model_name_or_path ='daryl149/llama-2-7b-hf'
 model_config = AutoConfig.from_pretrained(model_name_or_path )
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config)#.to(model_device)
 
 print("begin load model")
 ckpt_path = f"/home/wxt/.cache/huggingface/hub/llama2_7b_sft_halos_2_3/LATEST/policy.pt"
