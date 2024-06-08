ab234157a213070dd2b30225898c159e5922b093
Song Huatong <3151273556@qq.com>
2024-06-08 17:47:07 +0800

    Update check_code.py

--- train_code/check_code.py
+++ train_code/check_code.py
@@ -13,17 +13,26 @@ from transformers import (
     AutoModelForCausalLM,
     AutoModelForSequenceClassification
 )
-
+def delete_dict(d: Dict):
+    """Delete all items inside the dict."""
+    for k in list(d.keys()):
+        del d[k]
 import gc
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 import torch
 print(torch.cuda.device_count())
 
-# model_name_or_path ='daryl149/llama-2-7b-hf'
-# model_config = AutoConfig.from_pretrained(model_name_or_path )
-# model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
+model_name_or_path ='daryl149/llama-2-7b-hf'
+model_config = AutoConfig.from_pretrained(model_name_or_path )
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
 
-value_model = AutoModelForSequenceClassification.from_pretrained("OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5", num_labels=1)
-reward_model = AutoModelForSequenceClassification.from_pretrained("OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5", num_labels=1)
-    
+print("load model")
+ckpt_path = f"/home/wxt/.cache/huggingface/hub/{args.model_ckpt}/LATEST/policy.pt"
+state_dict = torch.load(ckpt_path, map_location='cpu')
+# step, metrics = state_dict['step_idx'], state_dict['metrics']
+model.load_state_dict(state_dict['state'])
+delete_dict(state_dict)
+gc.collect()
+torch.cuda.empty_cache()
+print('loaded pre-trained weights')
