523542606a7d60c188e4d4694bd4259df71874e4
Song Huatong <3151273556@qq.com>
2024-06-08 17:05:17 +0800

    Update train_ppo_trl.sh

--- train_code/train_ppo_trl.sh
+++ train_code/train_ppo_trl.sh
@@ -9,6 +9,9 @@ accelerate launch --config_file /home/wxt/huatong/TRL_FT/config_file/deepspeed_z
     --total_episodes 10000 \
     --model_name_or_path daryl149/llama-2-7b-hf \
     --sft_model_path daryl149/llama-2-7b-hf \
-    --reward_model_path OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5 \
+    --reward_model_path EleutherAI/pythia-1b-deduped \
     --local_rollout_forward_batch_size 1 \
     --non_eos_penalty 2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs.txt 
+
+
+    # -reward_model_path OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5 \
