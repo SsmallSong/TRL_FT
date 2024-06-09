4c75b34e2e9830dd438a6b6ad3ede95578c073d6
Song Huatong <3151273556@qq.com>
2024-06-09 23:19:51 +0800

    Update train_ppo_trl.sh

--- train_code/train_ppo_trl.sh
+++ train_code/train_ppo_trl.sh
@@ -1,5 +1,5 @@
-#accelerate launch --config_file /home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml \
-python	/home/wxt/huatong/TRL_FT/train_code/original_ppov2.py \
+accelerate launch --config_file /home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml \
+	/home/wxt/huatong/TRL_FT/train_code/original_ppov2.py \
     --output_dir /home/wxt/huatong/huggingface/hub/llama2_ppo_online \
     --num_ppo_epochs 1 \
     --num_mini_batches 1 \
@@ -10,7 +10,7 @@ python	/home/wxt/huatong/TRL_FT/train_code/original_ppov2.py \
     --response_length 100 \
     --model_name_or_path daryl149/llama-2-7b-hf \
     --sft_model_path daryl149/llama-2-7b-hf \
-    --reward_model_path "OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5"  \
+    --reward_model_path "cleanrl/EleutherAI_pythia-1b-deduped__reward__tldr"  \
     --local_rollout_forward_batch_size 1 \
     --stop_token eos \
     --stop_token_id 2 \
