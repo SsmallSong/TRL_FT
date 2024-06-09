d9345b6b7444d3b0a3c68799afb54087f4d55d06
Song Huatong <3151273556@qq.com>
2024-06-09 21:51:46 +0800

    Update check_code.py

--- train_code/check_code.py
+++ train_code/check_code.py
@@ -5,7 +5,7 @@ from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassifi
 model_name="OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 rm = AutoModelForSequenceClassification.from_pretrained(model_name)
-input_text = "<|prompter|>Hi how are you?<|endoftext|><|assistant|>Hi, I am Open-Assistant a large open-source language model trained by LAION AI. How can I help you today?<|endoftext|>"
+input_text = "<|user|>Hi how are you?<|assistant|>Hi, I am Open-Assistant a large open-source language model trained by LAION AI. How can I help you today?"
 inputs = tokenizer(input_text, return_tensors="pt")
 output=rm(**inputs)
 print(type(output))
