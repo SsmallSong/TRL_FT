7bb2cb9fe24db00532f2e89400aa2338b8660028
Song Huatong <3151273556@qq.com>
2024-06-09 13:45:41 +0800

    Update pt2bin.py

--- train_code/pt2bin.py
+++ train_code/pt2bin.py
@@ -1,18 +1,18 @@
 import torch
-with open('/home/wxt/huggingface/hub/llama2_sft_mirror/pytorch_model-00001-of-00002.bin', 'rb') as file:
-        binary_data_1 = file.read()
-with open('/home/wxt/huggingface/hub/llama2_sft_mirror/pytorch_model-00002-of-00002.bin', 'rb') as file:
-        binary_data_2 = file.read()
+bin_file_path = '/home/wxt/huggingface/hub/llama2_sft_mirror/pytorch_model-00001-of-00002.bin'  # 请将此路径替换为你的bin文件路径
+binary_data_1 = torch.load(bin_file_path, map_location='cpu')
+bin_file_path = '/home/wxt/huggingface/hub/llama2_sft_mirror/pytorch_model-00002-of-00002.bin'  # 请将此路径替换为你的bin文件路径
+binary_data_2 = torch.load(bin_file_path, map_location='cpu')
 num=0
 print(len(binary_data_1)+len(binary_data_2))
-for key , value in binary_data_1.items():
-    num+=1
-    if num==10:
-        break
-    print(key)
-    print(value)
+# for key , value in binary_data_1.items():
+#     num+=1
+#     if num==10:
+#         break
+#     print(key)
+#     print(value)
 
-#print(binary_data_1)
+print(binary_data_1)
 
 print("="*20)
 print("="*20)
@@ -29,13 +29,13 @@ cont=1
 print(len(policy))
 state_dict=policy['state']
 print(len(state_dict))
-#print((state_dict))
-# kill
-for key,value in state_dict.items():
-     cont+=1
-#     print("="*20)
-     print(key)
-     print(value)
-#     print("="*20)
-     if cont==10:
-         break
+print((state_dict))
+# # kill
+# for key,value in state_dict.items():
+#      cont+=1
+# #     print("="*20)
+#      print(key)
+#      print(value)
+# #     print("="*20)
+#      if cont==10:
+#          break
