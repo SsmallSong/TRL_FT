d991f5c6fb5bc39a2846ee6606db3bd72247403e
Song Huatong <3151273556@qq.com>
2024-06-09 21:48:36 +0800

    Update check_code.py

--- train_code/check_code.py
+++ train_code/check_code.py
@@ -2,9 +2,13 @@
 import model_training.models.reward_model  # noqa: F401 (registers reward model for AutoModel loading)
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
 
+model_name="OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 rm = AutoModelForSequenceClassification.from_pretrained(model_name)
 input_text = "<|prompter|>Hi how are you?<|endoftext|><|assistant|>Hi, I am Open-Assistant a large open-source language model trained by LAION AI. How can I help you today?<|endoftext|>"
 inputs = tokenizer(input_text, return_tensors="pt")
-score = rm(**inputs).logits[0].cpu().detach()
+output=rm(**inputs)
+print(type(output))
+print(output.keys())
+score = output.logits[0].cpu().detach()
 print(score)
