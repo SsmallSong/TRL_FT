d5d532b063ddf1a37255baca3974de2a86ecf91d
Song Huatong <3151273556@qq.com>
2024-06-09 13:57:32 +0800

    Update bin_change.py

--- train_code/bin_change.py
+++ train_code/bin_change.py
@@ -5,7 +5,7 @@ bin_file_path = '/home/wxt/huggingface/hub/llama2_sft_mirror/pytorch_model-00002
 binary_data_2 = torch.load(bin_file_path, map_location='cpu')
 
 print(len(binary_data_1)+len(binary_data_2))
-print(binary_data_1)
+# print(binary_data_1)
 set1 = set(binary_data_1.keys())
 set2 = set(binary_data_2.keys())
 bin_set = set1.union(set2)
@@ -19,4 +19,15 @@ policy =torch.load('/home/wxt/huatong/huggingface/hub/llama2_7b_sft_halos_2_3/LA
 print(len(policy))
 state_dict=policy['state']
 print(len(state_dict))
-print((state_dict))
+# print((state_dict))
+pt_set=set(state_dict.keys())
+
+# 找到 set1 中有但 set2 中没有的元素
+diff_set1 = bin_set - pt_set
+
+# 找到 set2 中有但 set1 中没有的元素
+diff_set2 = pt_set - bin_set
+
+# 打印差集合
+print("Elements in bin but not in pt:", diff_set1)
+print("Elements in pt but not in bin:", diff_set2)
