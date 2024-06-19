2945f9c590a460a13dd25f522fc79109465726a9
Song Huatong <3151273556@qq.com>
2024-06-19 11:18:27 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -2,8 +2,6 @@ export PYTHONIOENCODING=utf-8
 export OMP_NUM_THREADS=16
 
 index="llama2_7b_ppo_online"
-# index="llama2_7b_dpo_halos_beta01_frontenter"
-# index="llama2_7b_dpo_halos_beta01_endenter"
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_$index.log"
 model_ckpt="non"
 
@@ -13,13 +11,13 @@ accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
     --stage 1 > $log_file 2>&1 
      
     
-accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-    --index $index \
-    --stage 1 > $log_file 2>&1
+# accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+#     --index $index \
+#     --stage 1 > $log_file 2>&1
 
-python -u infer_and_eval_main_score.py \
-    --index $index \
-    --stage 1 > $log_file 2>&1
+# python -u infer_and_eval_main_score.py \
+#     --index $index \
+#     --stage 1 > $log_file 2>&1
 
 
 # index="llama2_7b_dpo_halos_beta01_frontenter"
@@ -39,19 +37,3 @@ python -u infer_and_eval_main_score.py \
 # #     --index $index \
 # #     --stage 1 > $log_file 2>&1
 
-
-# index="llama2_7b_dpo_halos_beta01_endenter"
-# log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_llama2_{index}.log"
-# model_ckpt="llama2_7b_dpo_halos_beta01"
-# accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
-#     --index $index  \
-#     --model_ckpt $model_ckpt
-#     --stage 1 > $log_file 2>&1 
-
-# # accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
-# #     --index $index \
-# #     --stage 1 > $log_file 2>&1
-
-# # python -u infer_and_eval_main_score.py \
-# #     --index $index \
-# #     --stage 1 > $log_file 2>&1
