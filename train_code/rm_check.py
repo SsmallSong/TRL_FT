0a36ca37184f246991ede53edc6f775eee8d8028
Song Huatong <3151273556@qq.com>
2024-06-20 17:39:00 +0800

    Update and rename check_code.py to rm_check.py

---
+++ train_code/rm_check.py
@@ -0,0 +1,25 @@
+# install open assistant model_training module (e.g. run `pip install -e .` in `model/` directory of open-assistant repository)
+import model_training.models.reward_model  # noqa: F401 (registers reward model for AutoModel loading)
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
+
+model_name="OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+rm = AutoModelForSequenceClassification.from_pretrained(model_name)
+input_text = "\n<|user|>\nHi how are you?\n<|assistant|>\nHi, I am Open-Assistant a large open-source language model trained by LAION AI. How can I help you today?"
+input_text_origin = "<|prompter|>Hi how are you?<|endoftext|><|assistant|>Hi, I am Open-Assistant a large open-source language model trained by LAION AI. How can I help you today?<|endoftext|>"
+
+inputs = tokenizer(input_text, return_tensors="pt")
+output=rm(**inputs)
+score = output.logits[0].cpu().detach()
+print("change prompt score: ",score)
+
+inputs = tokenizer(input_text_origin, return_tensors="pt")
+output=rm(**inputs)
+score = output.logits[0].cpu().detach()
+print("origin prompt score: ",score)
+
+batch_input=torch.tensor([input_text*5])
+inputs = tokenizer(batch_input, return_tensors="pt")
+output=rm(**inputs)
+score = output.logits[0].cpu().detach()
+print("batch score: ",score)
