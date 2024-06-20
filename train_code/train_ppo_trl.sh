6414c40ac00446f06035708e2cef77f359e4b2fb
Song Huatong <3151273556@qq.com>
2024-06-20 18:04:21 +0800

    Update train_ppo_trl.sh

--- train_code/train_ppo_trl.sh
+++ train_code/train_ppo_trl.sh
@@ -1,22 +1,28 @@
-accelerate launch --config_file /home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml \
-	/home/wxt/huatong/TRL_FT/train_code/original_ppov2.py \
-    --output_dir /home/wxt/huatong/huggingface/hub/llama2_ppo_online \
-    --num_ppo_epochs 1 \
-    --num_mini_batches 1 \
-    --learning_rate 3e-6 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 16 \
-    --total_episodes 10000 \
-    --response_length 100 \
-    --model_name_or_path daryl149/llama-2-7b-hf \
-    --sft_model_path daryl149/llama-2-7b-hf \
-    --reward_model_path "EleutherAI/pythia-1b-deduped"  \
-    --local_rollout_forward_batch_size 1 \
-    --stop_token eos \
-    --stop_token_id 2 \
-    --non_eos_penalty 2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs_sft.txt 
+# accelerate launch --config_file /home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml \
+# 	/home/wxt/huatong/TRL_FT/train_code/original_ppov2.py \
+#     --output_dir /home/wxt/huatong/huggingface/hub/llama2_ppo_online \
+#     --num_ppo_epochs 1 \
+#     --num_mini_batches 1 \
+#     --learning_rate 3e-6 \
+#     --per_device_train_batch_size 1 \
+#     --gradient_accumulation_steps 16 \
+#     --total_episodes 10000 \
+#     --response_length 100 \
+#     --model_name_or_path daryl149/llama-2-7b-hf \
+#     --sft_model_path daryl149/llama-2-7b-hf \
+#     --reward_model_path "EleutherAI/pythia-1b-deduped"  \
+#     --local_rollout_forward_batch_size 1 \
+#     --stop_token eos \
+#     --stop_token_id 2 \
+#     --non_eos_penalty 2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs_sft.txt 
 
 
-    # -reward_model_path OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5 \
-    #EleutherAI/pythia-1b-deduped
-#cleanrl/EleutherAI_pythia-1b-deduped__reward__tldr
+#     # -reward_model_path OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5 \
+#     #EleutherAI/pythia-1b-deduped
+# #cleanrl/EleutherAI_pythia-1b-deduped__reward__tldr
+
+python /home/wxt/huatong/TRL_FT/train_code/ppo_train.py \
+    --log_with=wandb \
+    --reward_model="OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5" \
+    --query_dataset="trl-internal-testing/hh-rlhf-trl-style" \
+    --model_name="/home/wxt/huggingface/hub/llama2_sft_mirror/"  2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/ppov1_logs_sft.txt 
