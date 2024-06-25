86e09fa462c2e598735e7b107fb4e5d747d7db8b
Song Huatong <3151273556@qq.com>
2024-06-25 23:04:39 +0800

    Update ppo_train.py

--- train_code/ppo_train.py
+++ train_code/ppo_train.py
@@ -183,3 +183,4 @@ for _epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
     # Run PPO step
     stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
     ppo_trainer.log_stats(stats, batch, rewards, columns_to_log=["query", "response", "ref_response", "ref_rewards"])
+torch.save(ppo_trainer.model.state_dict(), save_path)
