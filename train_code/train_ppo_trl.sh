673c4674aad5f985dc18c8c53a7fbabb5c63c6e1
Song Huatong <3151273556@qq.com>
2024-06-25 12:39:13 +0800

    Update train_ppo_trl.sh

--- train_code/train_ppo_trl.sh
+++ train_code/train_ppo_trl.sh
@@ -16,22 +16,22 @@
 #     --stop_token_id 2 \
 #     --non_eos_penalty 2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs_sft.txt 
 
-accelerate launch --config_file=/home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml  --main_process_port 8888\
-    --num_processes 4\
-    /home/wxt/huatong/TRL_FT/train_code/ppov2_train.py \
-    --output_dir  /home/wxt/huatong/huggingface/hub/llama2_ppo_online \
-    --num_ppo_epochs 1 \
-    --num_mini_batches 1 \
-    --learning_rate 3e-6 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 16 \
-    --total_episodes 10000 \
-    --model_name_or_path daryl149/llama-2-7b-hf \
-    --sft_model_path daryl149/llama-2-7b-hf \
-    --reward_model_path EleutherAI/pythia-1b-deduped \
-    --local_rollout_forward_batch_size 1 \
-    --deepspeed3 \
-    --non_eos_penalty  2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs.txt 
+# accelerate launch --config_file=/home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml  --main_process_port 8888\
+#     --num_processes 4\
+#     /home/wxt/huatong/TRL_FT/train_code/ppov2_train.py \
+#     --output_dir  /home/wxt/huatong/huggingface/hub/llama2_ppo_online \
+#     --num_ppo_epochs 1 \
+#     --num_mini_batches 1 \
+#     --learning_rate 3e-6 \
+#     --per_device_train_batch_size 1 \
+#     --gradient_accumulation_steps 16 \
+#     --total_episodes 10000 \
+#     --model_name_or_path daryl149/llama-2-7b-hf \
+#     --sft_model_path daryl149/llama-2-7b-hf \
+#     --reward_model_path EleutherAI/pythia-1b-deduped \
+#     --local_rollout_forward_batch_size 1 \
+#     --deepspeed3 \
+#     --non_eos_penalty  2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/llama2_ppo_logs.txt 
 
 #     # -reward_model_path OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5 \
 #     #EleutherAI/pythia-1b-deduped
@@ -43,14 +43,14 @@ accelerate launch --config_file=/home/wxt/huatong/TRL_FT/config_file/deepspeed_z
 #     --query_dataset="trl-internal-testing/hh-rlhf-trl-style" \
 #     --model_name="/home/wxt/huggingface/hub/llama2_sft_mirror/"  2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/ppov1_logs_sft.txt 
 
-# accelerate launch --config_file=/home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml  --main_process_port 8888 \
-#     /home/wxt/huatong/TRL_FT/train_code/ppo_train.py \
-#     --output_dir "/home/wxt/huatong/huggingface/hub/llama2_ppo_online_v1" \
-#     --mini_batch_size 32 \
-#     --learning_rate 3e-6 \
-#     --ppo_epochs 3 \
-#     --log_with=wandb \
-#     --reward_model="OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5" \
-#     --query_dataset="trl-internal-testing/hh-rlhf-trl-style" \
-#     --model_name="/home/wxt/huggingface/hub/llama2_sft_mirror/"  2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/ppov1_logs_sft.txt 
+accelerate launch --config_file=/home/wxt/huatong/TRL_FT/config_file/deepspeed_zero3.yaml  --main_process_port 8888 \
+    /home/wxt/huatong/TRL_FT/train_code/ppo_train.py \
+    --output_dict "/home/wxt/huatong/huggingface/hub/llama2_ppo_online_v1" \
+    --mini_batch_size 32 \
+    --learning_rate 3e-6 \
+    --ppo_epochs 3 \
+    --log_with=wandb \
+    --reward_model="OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5" \
+    --query_dataset="trl-internal-testing/hh-rlhf-trl-style" \
+    --model_name="/home/wxt/huggingface/hub/llama2_sft_mirror/"  2>&1 | tee /home/wxt/huatong/TRL_FT/train_code/ppov1_logs_sft.txt 
 
