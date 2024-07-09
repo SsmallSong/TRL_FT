ef38e1f44db66d00b6b4493ba4dd80ebd6ca92fc
Song Huatong <3151273556@qq.com>
2024-07-09 22:45:29 +0800

    Add files via upload

---
+++ test_code/model_another_test/count_rel.py
@@ -0,0 +1,271 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers
+from vllm import LLM, SamplingParams
+import json
+import numpy as np
+from tqdm import tqdm
+fs = []
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_mft5_rl0_eurus_1e-6_checkpoint-2000.json'
+# fs.append(f)
+# # 2294.0789
+# # 0.14317279
+#   17.18     10.83   
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.15_hh_len2048_tgt1024_win256checkpoint-400.json'
+# fs.append(f)
+# # 2297
+# # 0.14405085
+# 17.79 11.25
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.25_hh_len2048_tgt1024_win256checkpoint-400.json'
+# fs.append(f)
+# # 2276.7424
+# # 0.1438
+#  16.64     10.28  
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.15_hh_len2048_tgt1024_win256checkpoint-300.json'
+# fs.append(f)
+# 2253.107
+# 0.1435144
+# 17.14     10.50 
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_w0.2_hh_len2048_tgt1024_win256checkpoint-700.json'
+# fs.append(f)
+# 2297.0728
+# 0.14419
+#  16.20     10.38  
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.15_hh_len2048_tgt1024_win256checkpoint-500.json'
+# fs.append(f)
+# 2307.7683
+# 0.14428827
+# 17.33 10.64
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.25_hh_len2048_tgt1024_win256checkpoint-500.json'
+# fs.append(f)
+# 2281
+# 0.14402968
+#  17.42     10.55    
+
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.15_hh_len2048_tgt1024_win256checkpoint-1400.json'
+# fs.append(f)
+# 2201.8696
+# 0.1402
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_meanmbeta0.25_hh_len2048_tgt1024_win256checkpoint-1400.json'
+# fs.append(f)
+# 2276.9734
+# 0.14378971
+
+
+# f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyhermes_1e-6_64_mbeta0.15_hh_len2048_tgt1024_win256checkpoint-700.json'
+# fs.append(f)
+# # 2252.9375
+
+# 1295.2771
+# 
+# 3391.6226
+# 0.16634998
+f = '/mnt/data_local/tmp/llama/alpaca_mntdata_largeccyllama3_5e-7_64_mbeta0.15_hh_len2048_tgt1024_win256checkpoint-700.json'
+f = '/mnt/data_large/ccy/alpaca_mntdata_largeccyLlama-3-Instruct-8B-DPO.json'
+
+import argparse
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--model_id', type=str)
+    parser.add_argument('--force', type=bool, default=False)
+    args = parser.parse_args()
+    return args
+
+args = get_args()
+
+f = '/mnt/data_local/tmp/llama/alpaca_{}.json'.format(args.model_id.replace('/',''))
+
+
+fs.append(f)
+# 1e-5
+# 3091
+# 0.1576191
+ # 3e-6
+ # 31
+ # 0.15759987
+
+ # 3038.6191
+ # 0.1569
+
+# /mnt/data_local/tmp/llama/alpaca_mntdata_largeccyllama3_1e-6_mft_e3_len2048_tgt1024checkpoint-2000.json 
+ # 3100.9163
+ # 
+
+def count_eurus_reward(messes):
+    eu_reward_model = transformers.AutoModel.from_pretrained(
+                '/mnt/data_large/ccy/Eurus-RM-7b',
+                trust_remote_code=True
+            )
+    eu_reward_model.cuda()
+    eu_reward_model.eval()
+    eu_tokenizer = transformers.AutoTokenizer.from_pretrained(
+                    '/mnt/data_large/ccy/Eurus-RM-7b')
+    eu_tokenizer.truncation_side = "left"
+    batch_size=1
+    scores = []
+    for i in tqdm(range(0, len(messes), batch_size)):
+        batch_mess = messes[i:i + batch_size]
+        # batch_tgt = predicts[i:i + batch_size]
+        def get_mess(messes):
+            final_mess = []
+            for m in messes:
+                str_res = ""
+                for e in m:
+                    if e['role'] == 'system':
+                        str_res += '[INST] ' + e['content']
+                    if e['role'] == 'user':
+                        str_res += '[INST] ' + e['content']
+                    else:
+                        str_res += ' [/INST] ' + e['content']
+                final_mess.append(str_res)
+            mess = eu_tokenizer(final_mess, max_length=4096, truncation=True,
+                                    return_tensors="pt")
+            mess = {k: v.to(eu_reward_model.device) for k, v in mess.items()}
+            return mess
+
+        inputs = get_mess(batch_mess)
+        with torch.no_grad():
+            output = eu_reward_model(**inputs)
+            # batch_score = torch.sigmoid(output.float().view(-1)).detach().cpu().numpy()
+            batch_score = output.float().view(-1).detach().cpu().numpy().tolist()
+        scores.extend(batch_score)
+    print(np.mean(scores))
+    del eu_reward_model
+    torch.cuda.empty_cache()
+    return scores
+
+def count_llama3_reward(messes):
+
+    path = "/mnt/data_large/ccy/ArmoRM-Llama3-8B-v0.1"
+            
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(path, device_map='cuda', 
+                        trust_remote_code=True)
+
+    eu_reward_model = model
+    eu_reward_model.eval()
+    eu_tokenizer = transformers.AutoTokenizer.from_pretrained(
+                    path)
+    eu_tokenizer.truncation_side = "left"
+    batch_size=1
+    scores = []
+    for i in tqdm(range(0, len(messes), batch_size)):
+        batch_mess = messes[i:i + batch_size]
+        # batch_tgt = predicts[i:i + batch_size]
+        def get_mess(messes):
+            final_mess = []
+            for m in messes:
+                str_res = ""
+                for e in m:
+                    if e['role'] == 'system':
+                        str_res += '<|start_header_id|>user<|end_header_id|>\n\n' + e['content'] + '<|eot_id|>'
+                    if e['role'] == 'user':
+                        str_res += '<|start_header_id|>user<|end_header_id|>\n\n' + e['content'] + '<|eot_id|>'
+                    else:
+                        str_res += '<|start_header_id|>assistant<|end_header_id|>\n\n' + e['content'] + '<|eot_id|>'
+                final_mess.append(str_res)
+            mess = eu_tokenizer(final_mess, max_length=4096, truncation=True,
+                                    return_tensors="pt")
+            mess = {k: v.to(eu_reward_model.device) for k, v in mess.items()}
+            return mess
+
+        inputs = get_mess(batch_mess)
+        with torch.no_grad():
+            output = eu_reward_model(**inputs)
+            multi_obj_rewards = output.rewards.cpu().float() 
+            # The gating layer's output is conditioned on the prompt
+            gating_output = output.gating_output.cpu().float()
+            # The preference score for the response, aggregated from the 
+            # multi-objective rewards with the gating layer
+            preference_score = output.score.cpu().float().numpy().tolist()
+            batch_score = preference_score
+        scores.extend(batch_score)
+    print(np.mean(scores))
+    del eu_reward_model
+    torch.cuda.empty_cache()
+    return scores
+
+
+def count_hh_reward(messes):
+    import model_training.models.reward_model
+    from transformers import AutoTokenizer, AutoModelForSequenceClassification
+    model_name = 'OpenAssistant/oasst-rm-2.1-pythia-1.4b-epoch-2.5'
+    model_name = '/mnt/data_large/ccy/oasst-rm-2.1-pythia-1.4b-epoch-2.5'
+
+    #path = "/mnt/data_large/ccy/ArmoRM-Llama3-8B-v0.1"
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_name, device_map='cuda', 
+                        trust_remote_code=True)
+
+    eu_reward_model = model
+    eu_reward_model.eval()
+    eu_tokenizer = transformers.AutoTokenizer.from_pretrained(
+                    model_name)
+    eu_tokenizer.truncation_side = "left"
+    batch_size=1
+    scores = []
+    for i in tqdm(range(0, len(messes), batch_size)):
+        batch_mess = messes[i:i + batch_size]
+        # batch_tgt = predicts[i:i + batch_size]
+        def get_mess(messes):
+            final_mess = []
+            for m in messes:
+                str_res = ""
+                for e in m:
+                    if e['role'] == 'assistant':
+                        str_res += '<|assistant|>{}{}'.format(e['content'], eu_tokenizer.eos_token)
+                    else:
+                        str_res += '<|prompter|>{}{}'.format(e['content'], eu_tokenizer.eos_token)
+                final_mess.append(str_res)
+            mess = eu_tokenizer(final_mess, max_length=4096, truncation=True,
+                                    return_tensors="pt")
+            mess = {k: v.to(eu_reward_model.device) for k, v in mess.items()}
+            return mess
+
+        inputs = get_mess(batch_mess)
+        with torch.no_grad():
+            output = eu_reward_model(**inputs)
+            batch_score = output.logits.float().view(-1).detach().cpu().numpy().tolist()
+        scores.extend(batch_score)
+    print(np.mean(scores))
+    del eu_reward_model
+    torch.cuda.empty_cache()
+    return scores
+
+for f in fs:
+    x = json.load(open(f, 'r'))
+    messes = []
+    for e in tqdm(x):
+        messes.append([{'role': 'user', 'content': e['instruction']}, {'role': 'assistant', 'content': e['output'].replace('<|eot_id|>', '')}])
+    
+    print(args.model_id)
+    # if 'eurus' not in x[0]:
+    #     scores = count_eurus_reward(messes)
+    #     for i, e in enumerate(x):
+    #         x[i]['eurus'] = scores[i]
+    # else:
+    #     print(np.mean([e['eurus'] for e in x]))
+    if 'llama3' not in x[0]:
+        scores = count_llama3_reward(messes)
+        for i, e in enumerate(x):
+            x[i]['llama3'] = scores[i]
+    else:
+        print(np.mean([e['llama3'] for e in x]))
+    if 'eurus' not in x[0] or args.force:
+        scores = count_eurus_reward(messes)
+        for i, e in enumerate(x):
+            x[i]['eurus'] = scores[i]
+        json.dump(x, open(f, 'w'))
+    else:
+        print(np.mean([e['eurus'] for e in x]))
+
+    if 'hh' not in x[0] or args.force:
+        scores = count_hh_reward(messes)
+        for i, e in enumerate(x):
+            x[i]['hh'] = scores[i]
+        json.dump(x, open(f, 'w'))
+    else:
+        print(np.mean([e['hh'] for e in x]))
+    
+    #count_llama3_reward(messes)
+    
+
+    
---
+++ test_code/model_another_test/eval_res.py
@@ -0,0 +1,408 @@
+import datasets
+import json
+import os
+import numpy as np
+import random
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers
+from vllm import LLM, SamplingParams
+import pickle as pkl
+
+f = '../llama/FastChat/fastchat/llm_judge/data/mt_bench/question.jsonl'
+x = open(f).readlines()
+sampling_params = SamplingParams(temperature=0, max_tokens=2048, n=1)
+
+import argparse
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--model_id', type=str)
+    args = parser.parse_args()
+    return args
+
+args = get_args()
+
+model_id = ""
+ref_model_id = ""
+ref_model_id = '/mnt/data_large/ccy/hermes_mft2_ray_rl0_0.2_7_not_nor2_lora_checkpoint_6000'
+model_id = '/mnt/data_large/ccy/hermes_mft2_ray_rl0_0.2_7_not_nor2_lora_checkpoint_6000'
+model_id = '/mnt/data_large/ccy/hermes_mft2_ray_rl1_0.2_7_not_nor2_lora_hh_checkpoint_6500'
+model_id = '/mnt/data_large/ccy/hermes_mft2_ray_rl0.5_0.2_15_not_nor2_lora_eurus_checkpoint_1500'
+model_id = '/mnt/data_large/ccy/hermes_mft4/checkpoint-3000'
+model_id = '/mnt/data_large/ccy/hermes_mft2_ray_rl0.5_0.25_15_not_nor2_lora_eurus_5e-5_checkpoint_2000'
+model_id = '/mnt/data_large/ccy/hermes_sft_tool_base_2/checkpoint-16500'
+model_id = '/mnt/data_large/ccy/mistral_rft/checkpoint-450'
+model_id = '/mnt/data_large/ccy/mistral_rft_5e-6_64_capy_rl_w0.05_hh_128/checkpoint-236'
+model_id = '/mnt/data_large/ccy/mistral_rft_5e-6_64_capy_rl_w0.25_hh_63_len512/checkpoint-474'
+model_id = '/mnt/data_large/ccy/mistral_rft_5e-6_64_capy_rl_w0_hh_63_len512/checkpoint-177'
+model_id = '/mnt/data_large/ccy/mistral_rft_5e-6_64_capy_warm33_cont/checkpoint-1180'
+model_id = '/mnt/data_large/ccy/llama2_score_0.6'
+model_id = '/mnt/data_large/ccy/Meta-Llama-3-8B-Instruct'
+model_id = '/mnt/data_large/ccy/Llama-3-Instruct-8B-DPO'
+model_id = args.model_id
+mistral_temp = False
+capy_temp = False
+hh_temp = False
+alpaca_temp = False
+llama3_temp = True
+res = []
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_id)
+
+def gen_templated_instruction(prefix, suffix=''):
+    template = '''Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{instruction}
+
+### Response:
+{response}'''
+    return template.format(instruction=prefix, response=suffix)
+    
+if capy_temp:
+    tokenizer = AutoTokenizer.from_pretrained("/mnt/data_large/ccy/mistral-orpo-capybara-7k")
+
+'''
+if not os.path.exists('mess_{}'.format(model_id.replace('/',''))):
+    llm = LLM(model=model_id, tensor_parallel_size=1,
+                  trust_remote_code=True)
+    pos = model_id.rfind('/')
+    name = model_id[pos:]
+    if name.find('checkpoint') == 1:
+        name = model_id[model_id.rfind('/', 0, pos):].replace('/', '')
+    messes = []
+    for e in x[:]:
+        e = json.loads(e)
+        turns = e['turns']
+        question_id = e['question_id']
+        mess = []
+        resp_turns = []
+        for t in turns:
+            mess.append({'role': 'user', 'content': t})
+            if alpaca_temp:
+                if t == 0:
+                    prompts = gen_templated_instruction(mess[0]['content'])
+                else:
+                    prompts = gen_templated_instruction(str(mess))
+            else:
+                prompts = tokenizer.apply_chat_template(mess, tokenize=False, add_generation_prompt=True)
+            # if not mistral_temp and not capy_temp:
+            #     prompts += "<|im_start|>assistant\n"
+            # print(prompts)
+            # ___ = input()
+
+            predict = llm.generate(prompts, sampling_params)
+            mess.append({'role': 'assistant', 'content': predict[0].outputs[0].text.strip()})
+            resp_turns.append(predict[0].outputs[0].text.strip())
+            # prefix = "<|im_start|>assistant\n{}<|im_end|>\n".format(t)
+        messes.append(mess)
+        res.append({'question_id': question_id,  'answer_id': question_id, 'model_id': name,
+                    'choices': [{'index': 0, 'turns': resp_turns}]})
+        res[-1] = json.dumps(res[-1])
+
+    out_f = '../llama/FastChat/fastchat/llm_judge/data/mt_bench/model_answer/{}.jsonl'.format(name)
+    out_f = open(out_f, 'w')
+    out_f.write('\n'.join(res))
+
+    pkl.dump(messes, open('mess_{}'.format(model_id.replace('/', '')),'wb'))
+
+    del llm
+    torch.cuda.empty_cache()
+
+else:
+    prompts = []
+    messes = pkl.load(open('mess_{}'.format(model_id.replace('/', '')), 'rb'))
+    for i, e in enumerate(x[:]):
+        e = json.loads(e)
+        turns = e['turns']
+        question_id = e['question_id']
+        # mess = []
+        resp_turns = []
+        for j, t in enumerate(turns):
+            mess = messes[i][:j*2+1]
+            prompt = tokenizer.apply_chat_template(mess, tokenize=False)
+            prompt += "<|im_start|>assistant\n"
+            prompts.append(prompt)
+
+
+new_messes = []
+for e in messes:
+    new_messes.append(e)
+    new_messes.append(e[:2])
+messes = new_messes
+
+'''
+
+
+
+if not os.path.exists('alpaca_{}.json'.format(model_id.replace('/', ''))):
+    llm = LLM(model=model_id, tensor_parallel_size=1,
+                      trust_remote_code=True)
+    import datasets
+    eval_set = datasets.load_dataset("mt-bench/alpaca_eval", "alpaca_eval")["eval"]
+    alpaca_prompts = []
+    for example in eval_set:
+        if hh_temp:
+            alpaca_prompts.append('\n\nHuman: {}\n\nAssistant:'.format(example["instruction"]))
+        elif mistral_temp:
+            alpaca_prompts.append('[INST] {} [\INST]'.format(example["instruction"]))
+        elif capy_temp:
+            alpaca_prompts.append('<|user|>\n{}</s>\n<|assistant|>\n'.format(example["instruction"]))
+        elif alpaca_temp:
+            alpaca_prompts.append(gen_templated_instruction(example["instruction"]))
+        elif llama3_temp:
+            prompt = tokenizer.apply_chat_template([{'role': 'user', 'content': example['instruction']}], tokenize=False, add_generation_prompt=True)
+            alpaca_prompts.append(prompt)
+        else:
+            alpaca_prompts.append('<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant\n'.format(example["instruction"]))
+
+
+    res = []
+    alpaca_predicts = llm.generate(alpaca_prompts, sampling_params)
+    for i, example in enumerate(eval_set):
+        example["output"] = alpaca_predicts[i].outputs[0].text.strip()
+        example['output'] = example['output'].replace('<|im_start|>','')
+        res.append(example)
+    alpaca_out = open('alpaca_{}.json'.format(model_id.replace('/', '')), 'w')
+    json.dump(res, alpaca_out, ensure_ascii=False, indent=4)
+    # alpaca_out.write('\n'.join(res))
+    del llm
+    torch.cuda.empty_cache()
+
+
+
+messes = []
+with open('alpaca_{}.json'.format(model_id.replace('/', '')), 'r', encoding='utf-8') as file:
+    res = json.load(file)
+
+for e in res:
+    messes.append([{'role': 'user', 'content': e['instruction']}, {'role': 'assistant', 'content': e['output']}])
+
+print(len(messes))
+print(messes[0])
+
+
+ray_reward_model = transformers.AutoModelForSequenceClassification.from_pretrained(
+                '/mnt/data_large/ccy/reward-model-Mistral-7B-instruct-Unified-Feedback',
+                num_labels=1,
+                torch_dtype=torch.bfloat16,
+                # load_in_4bit=True,
+                # bnb_4bit_compute_dtype=torch.bfloat16
+            )
+ray_reward_model.cuda()
+ray_reward_model.eval()
+ray_tokenizer = transformers.AutoTokenizer.from_pretrained('/mnt/data_large/ccy/reward-model-Mistral-7B-instruct-Unified-Feedback')
+ray_tokenizer.truncation_side = "left"
+ray_tokenizer = transformers.AutoTokenizer.from_pretrained(
+    '/mnt/data_large/ccy/reward-model-Mistral-7B-instruct-Unified-Feedback')
+
+batch_size=4
+scores = []
+for i in range(0, len(messes), batch_size):
+    batch_mess = messes[i:i + batch_size]
+    # batch_tgt = predicts[i:i + batch_size]
+    def get_mess(mess):
+        mess = [ray_tokenizer.apply_chat_template(e, tokenize=False) for e in mess]
+        # mess = self.ray_tokenizer.encode_plus(mess, padding=True, max_length=4096, truncation=True, return_tensors="pt")
+        mess = ray_tokenizer(mess, padding=True, max_length=4096, truncation=True,
+                                  return_tensors="pt")
+        mess = {k: v.to(ray_reward_model.device) for k, v in mess.items()}
+
+        return mess
+
+    inputs = get_mess(batch_mess)
+    with torch.no_grad():
+        output = ray_reward_model(**inputs)
+
+        # batch_score = torch.sigmoid(output.logits.float().view(-1)).detach().cpu().numpy()
+        batch_score = output.logits.float().view(-1).detach().cpu().numpy()
+    scores.extend(batch_score)
+
+
+
+
+f = '../llama/FastChat/fastchat/llm_judge/data/mt_bench/question.jsonl'
+x = open(f).readlines()
+scores_dict = {}
+for i, e in enumerate(x[:]):
+    e = json.loads(e)
+    cate = e['category']
+    if cate not in scores_dict:
+        scores_dict[cate] = []
+    scores_dict[cate].append(scores[i])
+
+for k, v in scores_dict.items():
+    scores_dict[k] = np.mean(scores_dict[k])
+
+print(scores_dict)
+print(np.mean(scores))
+
+del ray_reward_model
+torch.cuda.empty_cache()
+
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import model_training.models.reward_model
+
+model_name = '/mnt/data_large/ccy/oasst-rm-2.1-pythia-1.4b-epoch-2.5'
+hh_tokenizer = AutoTokenizer.from_pretrained(model_name)
+#self.hh_reward_model = AutoModelForSequenceClassification.from_pretrained(model_name, load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16)
+hh_reward_model = AutoModelForSequenceClassification.from_pretrained(model_name, torch_dtype=torch.bfloat16,)
+hh_reward_model.cuda()
+hh_reward_model.eval()
+hh_tokenizer.truncation_side = "left"
+
+
+
+batch_size=4
+scores = []
+for i in range(0, len(messes), batch_size):
+    batch_mess = messes[i:i + batch_size]
+    # batch_tgt = predicts[i:i + batch_size]
+    def get_mess(messes):
+        str_res = []
+        for mess in messes:
+            res = ""
+            for e in mess:
+                if e['role'] == 'assistant':
+                    res += '<|assistant|>{}{}'.format(e['content'], hh_tokenizer.eos_token)
+                else:
+                    res += '<|prompter|>{}{}'.format(e['content'], hh_tokenizer.eos_token)
+            str_res.append(res)
+        mess = hh_tokenizer(str_res, padding=True, max_length=2048, truncation=True,
+                                 return_tensors="pt")
+        mess = {k: v.to(hh_reward_model.device) for k, v in mess.items()}
+        return mess
+
+    inputs = get_mess(batch_mess)
+    with torch.no_grad():
+        output = hh_reward_model(**inputs)
+        # batch_score = torch.sigmoid(output.logits.float().view(-1)).detach().cpu().numpy()
+        batch_score = output.logits.float().view(-1).detach().cpu().numpy()
+    scores.extend(batch_score)
+
+f = '../llama/FastChat/fastchat/llm_judge/data/mt_bench/question.jsonl'
+x = open(f).readlines()
+scores_dict = {}
+for i, e in enumerate(x[:]):
+    e = json.loads(e)
+    cate = e['category']
+    if cate not in scores_dict:
+        scores_dict[cate] = []
+    scores_dict[cate].append(scores[i])
+
+for k, v in scores_dict.items():
+    scores_dict[k] = np.mean(scores_dict[k])
+
+print(scores_dict)
+print(np.mean(scores))
+del hh_reward_model
+torch.cuda.empty_cache()
+
+
+'''
+'''
+eu_reward_model = transformers.AutoModel.from_pretrained(
+                '/mnt/data_large/ccy/Eurus-RM-7b',
+                torch_dtype=torch.bfloat16,
+                trust_remote_code=True
+            )
+eu_reward_model.cuda()
+eu_reward_model.eval()
+eu_tokenizer = transformers.AutoTokenizer.from_pretrained(
+                '/mnt/data_large/ccy/Eurus-RM-7b')
+eu_tokenizer.truncation_side = "left"
+
+batch_size=1
+scores = []
+for i in range(0, len(messes), batch_size):
+    batch_mess = messes[i:i + batch_size]
+    # batch_tgt = predicts[i:i + batch_size]
+    def get_mess(messes):
+        final_mess = []
+        for m in messes:
+            str_res = ""
+            for e in m:
+                if e['role'] == 'system':
+                    str_res += '[INST] ' + e['content']
+                if e['role'] == 'user':
+                    str_res += '[INST] ' + e['content']
+                else:
+                    str_res += ' [\INST] ' + e['content']
+            final_mess.append(str_res)
+        mess = eu_tokenizer(final_mess, max_length=4096, truncation=True,
+                                 return_tensors="pt")
+        mess = {k: v.to(eu_reward_model.device) for k, v in mess.items()}
+        return mess
+
+    inputs = get_mess(batch_mess)
+    with torch.no_grad():
+        output = eu_reward_model(**inputs)
+        # batch_score = torch.sigmoid(output.float().view(-1)).detach().cpu().numpy()
+        batch_score = output.float().view(-1).detach().cpu().numpy()
+    scores.extend(batch_score)
+print(np.mean(scores))
+
+del eu_reward_model
+torch.cuda.empty_cache()
+'''
+'''
+'''
+source_texts = prompts
+target_texts = [e[-1]['content'] for e in messes]
+model1 = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id
+)
+model1.eval()
+model1.cuda()
+def batch_target_probability(model, tokenizer, source_texts, target_texts):
+    # 对“source”文本进行编码
+    # tokenizer.padding_side = "left"
+    #tokenizer.truncation_side = "left"
+    source_encodings = tokenizer(source_texts, return_tensors="pt", padding=True, truncation=True)
+
+    # 对“target”文本进行编码，并计算概率
+    # tokenizer.padding_side = "right"
+    target_encodings = tokenizer(target_texts, return_tensors="pt", padding=True, truncation=True,
+                                 add_special_tokens=False)
+    source_encodings = {k: v.to(model.device) for k, v in source_encodings.items()}
+    target_encodings = {k: v.to(model.device) for k, v in target_encodings.items()}
+
+    with torch.no_grad():
+        inputs = {key: torch.cat([source_encodings[key], target_encodings[key]], dim=-1) for key in source_encodings}
+        labels = torch.cat([torch.full_like(source_encodings['input_ids'], -100), target_encodings['input_ids']],
+                           dim=-1)
+        outputs = model(**inputs, labels=labels)
+        loss = outputs.loss
+        return torch.exp(-loss)  # 计算“target”部分的概率
+
+probs1 = []
+for s, t in zip(source_texts, target_texts):
+    probs1.append(batch_target_probability(model1, tokenizer, s, t))
+
+
+
+del model1
+torch.cuda.empty_cache()
+# ref_model_id = ''
+model2 = transformers.AutoModelForCausalLM.from_pretrained(
+    ref_model_id
+)
+model2.eval()
+model2.cuda()
+probs2 = []
+for s, t in zip(source_texts, target_texts):
+    probs2.append(batch_target_probability(model2, tokenizer, s, t))
+# 计算KL散度（向量化操作）
+
+probs2 = torch.tensor(probs2)
+probs1= torch.tensor(probs1)
+
+
+epsilon = 1e-10
+probs1 += epsilon
+probs2 += epsilon
+kl_divergence = torch.sum(probs1 * torch.log(probs1 / probs2))
+print("总KL散度为:", kl_divergence.item())
+'''
+#probs = batch_target_probability(model, tokenizer, source_texts, target_texts)
