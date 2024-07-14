13bfcf881aa5c7e08dfe2af66183f199ca765652
Song Huatong <3151273556@qq.com>
2024-07-14 09:29:55 +0800

    Update run_llama2.sh

--- test_code/PRO/eval_hh/run_llama2.sh
+++ test_code/PRO/eval_hh/run_llama2.sh
@@ -1,9 +1,9 @@
 export PYTHONIOENCODING=utf-8
 export OMP_NUM_THREADS=16
 
-index="7b_llama_ppo_openrlhf"
+index="7b_llama_dpo_beta001_lr1e6"
 log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_$index.log"
-model_ckpt="non"
+model_ckpt="llama2_7b_dpo_halos_beta001_lr1e6"
 
 accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
     --index $index \
@@ -17,7 +17,25 @@ accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
 
 python -u infer_and_eval_main_score.py \
     --index $index \
+    --stage 1 > $log_file 2>&1 
+    
+index="7b_llama_dpo_beta001_lr2e6"
+log_file="/home/wxt/huatong/TRL_FT/test_code/PRO/eval_hh/logs_new/generate_infer_main_$index.log"
+model_ckpt="llama2_7b_dpo_halos_beta001_lr2e6"
+
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_generate.py \
+    --index $index \
+    --model_ckpt $model_ckpt \
     --stage 1 
+     
+    
+accelerate launch --config_file dp_config.yaml infer_and_eval_main_reward.py \
+    --index $index \
+    --stage 1 
+
+python -u infer_and_eval_main_score.py \
+    --index $index \
+    --stage 1 > $log_file 2>&1 
 
 
 # index="llama2_7b_dpo_halos_beta01_frontenter"
