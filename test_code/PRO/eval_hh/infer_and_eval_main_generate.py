d00333a05855aea8eae824666b42edd4e79ed176
Song Huatong <3151273556@qq.com>
2024-07-14 09:19:52 +0800

    Update infer_and_eval_main_generate.py

--- test_code/PRO/eval_hh/infer_and_eval_main_generate.py
+++ test_code/PRO/eval_hh/infer_and_eval_main_generate.py
@@ -42,23 +42,23 @@ if __name__ == "__main__":
     accelerator = Accelerator(kwargs_handlers=[kwargs])# **accelerator_log_kwargs)
     rank = int(os.environ['RANK'])
     rank_sum = accelerator.num_processes
-    # model_name_or_path = os.path.join("..", "checkpoints", f"index_{args.index}", f"stage_{args.stage}", f"{args.directory}")
-    model_name_or_path ='/home/wxt/.cache/huggingface/hub/7b_llama_ppo_openrlhf'
+    model_name_or_path = os.path.join("..", "checkpoints", f"index_{args.index}", f"stage_{args.stage}", f"{args.directory}")
+    # model_name_or_path ='/home/wxt/.cache/huggingface/hub/7b_llama_ppo_openrlhf'
     model_device = "cuda:{}".format(rank)
 
     model_config = AutoConfig.from_pretrained(model_name_or_path )
     model = AutoModelForCausalLM.from_pretrained(model_name_or_path,config=model_config).to(model_device)
 
-    # if args.model_ckpt!='non':
-    #     print("load model")
-    #     ckpt_path = f"/home/wxt/.cache/huggingface/hub/{args.model_ckpt}/LATEST/policy.pt"
-    #     state_dict = torch.load(ckpt_path, map_location='cpu')
-    #     # step, metrics = state_dict['step_idx'], state_dict['metrics']
-    #     model.load_state_dict(state_dict['state'])
-    #     delete_dict(state_dict)
-    #     gc.collect()
-    #     torch.cuda.empty_cache()
-    #     print('loaded pre-trained weights')
+    if args.model_ckpt!='non':
+        print("load model")
+        ckpt_path = f"/home/wxt/.cache/huggingface/hub/{args.model_ckpt}/LATEST/policy.pt"
+        state_dict = torch.load(ckpt_path, map_location='cpu')
+        # step, metrics = state_dict['step_idx'], state_dict['metrics']
+        model.load_state_dict(state_dict['state'])
+        delete_dict(state_dict)
+        gc.collect()
+        torch.cuda.empty_cache()
+        print('loaded pre-trained weights')
 
     if accelerator.is_main_process:
         print(type(model))
